f7d8746667186ac76ca4aa99e48b10dabfc1c1bc
Gêdhean Alves Vieira <gedhean1@gmail.com>
2018-03-30 11:21:03 -0300

    gitignore package installation added

--- config-npm.sh
+++ config-npm.sh
@@ -1,13 +1,10 @@
 #npm configuration by Gêdhean Alves Vieira
-
-AUTHOR_NAME="Gêdhean Alves Vieira"
-AUTHOR_EMAIL="gedhean1@gmail.com"
-
-LICENSE="MIT"
+#install gitignore
+#npm install gitignore -g
 
 npm set init-author-name "Gêdhean Alves Vieira"
 npm set init-author-email "gedhean1@gmail.com"
-npm set init-license $LICENSE
+npm set init-license "MIT"
 
 npm get init-author-name
 npm get init-author-email
