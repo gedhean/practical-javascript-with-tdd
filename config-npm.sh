3c03801f9a1c24f4ae43be361f7e9b987a695db0
Gêdhean Alves Vieira <gedhean1@gmail.com>
2018-03-30 10:51:26 -0300

    added npm config script

---
+++ config-npm.sh
@@ -0,0 +1,14 @@
+#npm configuration by Gêdhean Alves Vieira
+
+AUTHOR_NAME="Gêdhean Alves Vieira"
+AUTHOR_EMAIL="gedhean1@gmail.com"
+
+LICENSE="MIT"
+
+npm set init-author-name "Gêdhean Alves Vieira"
+npm set init-author-email "gedhean1@gmail.com"
+npm set init-license $LICENSE
+
+npm get init-author-name
+npm get init-author-email
+npm get init-license
